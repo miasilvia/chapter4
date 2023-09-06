d7f36472660d41ab329e7f3d866fb4e43010cf2e
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-09-06 23:48:48 +0700

    first commit

---
+++ looping.js
@@ -0,0 +1,37 @@
+//for (i=1; i<=100; i++){
+//console.log(i)
+//}
+
+
+
+// let bintang="";
+// for (f=0; f<=5; f++){
+//     for(j=0; j<f; j++){
+//         bintang += "*";
+//     }
+//     bintang += "\n";
+// }
+// console.log(bintang);
+//////////////////////////////
+
+let bintang2="";
+for (f=5; f>=0; f--){
+    for(j=0; j<f; j++){
+        bintang2 += "*";
+    }
+    bintang2 += "\n";
+}
+console.log(bintang2);
+
+/** iterasi 1
+ f = 5
+ 5>=0 true
+    iterasi 1
+    j=0
+    j<f
+    0<5 true
+    tambahkan *
+    
+
+ */
+
