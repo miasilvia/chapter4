051914366316233bd3f1771cf50494ceded03507
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-09-07 23:56:22 +0700

    add bintang diamond

--- looping.js
+++ looping.js
@@ -14,14 +14,14 @@
 // console.log(bintang);
 //////////////////////////////
 
-let bintang2="";
-for (f=5; f>=0; f--){
-    for(j=0; j<f; j++){
-        bintang2 += "*";
-    }
-    bintang2 += "\n";
-}
-console.log(bintang2);
+// let bintang2="";
+// for (f=5; f>=0; f--){
+//     for(j=0; j<f; j++){
+//         bintang2 += "*";
+//     }
+//     bintang2 += "\n";
+// }
+// console.log(bintang2);
 
 /** iterasi 1
  f = 5
@@ -34,4 +34,34 @@ console.log(bintang2);
     
 
  */
+console.log("----------------------SOAL BINTANG DIAMOND------------------------------")
+let bintang3="";
+for (let i = 0; i<4; i++){
+  for(let j=5 - 1; j>=i; j--){
+    bintang3 += " ";
+  }
+  for(let k=1; k<=i; k++){
+    bintang3 += "*"
+  }
+  for(let l=1 - 1; l<=i; l++){
+    bintang3 += "*"
+  }
+  bintang3 += "\n"
+ 
+}
 
+let bintang4="";
+for (let i = 1; i<=5; i++){
+  for(let j=0 + 1; j<=i; j++){
+    bintang4 += " ";
+  }
+  for(let k=5; k>=i; k--){
+    bintang4 += "*"
+  }
+  for(let l=5-1 ; l>=i; l--){
+    bintang4 += "*"
+  }
+  bintang4 += "\n"
+ 
+}
+console.log(bintang3 + bintang4)
